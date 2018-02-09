4d7a71a301197f027aab3fb948ed0abb31dd8b8a
memsharded <james@conan.io>
2018-02-09 11:52:13 +0100

    updated CI

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="memsharded", channel="testing")
-    builder.add_common_builds(shared_option_name="ZMQ:shared")
+    builder = ConanMultiPackager()
+    builder.add_common_builds(shared_option_name="OpenCV:shared")
     builder.run()
