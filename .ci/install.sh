4128dbc8031da58eb351cd56d6bea2252040d655
SSE4 <tomskside@gmail.com>
2019-04-07 16:08:58 +0700

    - update CI files
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
-set -x
+set -ex
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
@@ -13,8 +12,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
     pyenv rehash
     pyenv activate conan
 fi
